f63bfd4dffd530bb1aa57dca2748b1dd097d93b8
roroarij <rosenarij@gmail.com>
2023-01-23 17:54:55 -0500

    added projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -13,6 +13,36 @@ const projectsData = [
     imgSrc: '/static/images/gpt.webp',
     href: '/blog/chatgpt-chrome-extension',
   },
+  {
+    title: 'Obit-Graph: Entity Extraction From Public Records',
+    description: `Building an NLP pipeline in python to extract entities from online obituaries and determine their relationships`,
+    imgSrc: '/static/images/gpt.webp',
+    href: '/blog/python-entity-extraction',
+  },
+  {
+    title: 'Going Serverless Part 1: ETL with Modal ',
+    description: `Designing a serverless ETL pipline in Python to extract business data from yellowpages`,
+    imgSrc: '/static/images/gpt.webp',
+    href: '/blog/going-serverless-1',
+  },
+  {
+    title: 'Gis.sh | Automatic GIS Layer Feature Extraction',
+    description: `Scraping County GIS endpoints for parcel data and more.`,
+    imgSrc: '/static/images/gpt.webp',
+    href: '/blog/gis-extraction',
+  },
+  {
+    title: 'Lava Financial | A Revenue Sharing Platform on Avalanche',
+    description: `Building and releasing a Dapp on Avalanche`,
+    imgSrc: '/static/images/gpt.webp',
+    href: '/blog/lava',
+  },
+  {
+    title: 'Prprty: A Real Estate Analytics Dashboard',
+    description: `Building a web app to collate and display useful information from multiple County websites`,
+    imgSrc: '/static/images/gpt.webp',
+    href: '/blog/prpty',
+  },
 ]
 
 export default projectsData

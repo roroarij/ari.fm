c3fa15a151c286c41443d14d2055a3c7fbfb5345
roroarij <rosenarij@gmail.com>
2023-01-23 17:00:15 -0500

    edit

--- data/projectsData.js
+++ data/projectsData.js
@@ -10,7 +10,7 @@ const projectsData = [
   {
     title: 'ChatGPT Chrome Extension',
     description: `Building an email-extracting chrome extension using OpenAI's ChatGPT model.`,
-    imgSrc: 'https://i0.wp.com/bdtechtalks.com/wp-content/uploads/2022/12/openai-chatgpt.jpg?resize=696%2C435&ssl=1',
+    imgSrc: '/static/images/gpt.webp',
     href: '/blog/chatgpt-chrome-extension',
   },
 ]
